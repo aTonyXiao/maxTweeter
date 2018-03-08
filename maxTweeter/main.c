e4da35eaca4d15b6100bf41ff5a23f837c6791a4
aTonyXiao <tony.xiao95@icloud.com>
2018-03-07 18:49:17 -0800

    Initial Commit

---
+++ maxTweeter/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  maxTweeter
+//
+//  Created by Tony Xiao on 3/7/18.
+//  Copyright © 2018 TonyXiao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
